dd83fe9042254834872010cd8851863e50f5cda7
ADARSHsri2004 <adarshsri619@gmail.com>
2024-11-05 12:58:05 +0530

    update

--- REACT/router_practice/src/components/Products.jsx
+++ REACT/router_practice/src/components/Products.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Link,NavLink,Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
 
 const Products = () => {
-  return (
-    <>
-    <div>
-      <input className='m-4 bg-black/10 p-2' placeholder='Search products' />
-    </div>
-      <nav className='flex gap-5 m-5'>
-        <Link to='/featured'>Featured</Link>
-        <Link to='/new'>New</Link>
-      </nav>
-      <Outlet/>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <input className='m-4 bg-black/10 p-2' placeholder='Search products' />
+            </div>
+            <nav className='flex gap-5 m-5'>
+                <Link to='featured'>Featured</Link>
+                <Link to='new'>New</Link>
+            </nav>
+            <Outlet />
+        </>
+    )
 }
 
 export default Products
